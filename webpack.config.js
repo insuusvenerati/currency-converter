1905b13289d27cc89a8e559929463b49a3366024
Sean Norwood <norwood.sean@gmail.com>
2023-08-13 13:51:36 -0500

    Inject assets into head so they load quicker

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Shape Tracker",
       template: "./src/index.html",
-      inject: "body",
+      inject: "head",
     }),
     new Dotenv(),
   ],
